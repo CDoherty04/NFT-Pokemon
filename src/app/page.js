4f6da2b1a027fb18aac12fb18f25fa485db86451
Charlie <charlieedoherty@gmail.com>
2025-08-17 04:25:02 -0400

    Add dynamic chains

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,6 @@ import { ArrowLeft } from 'lucide-react';
 
 // Setting up list of evmNetworks
 const evmNetworks = [
-  // Flow EVM Testnet
   {
     blockExplorerUrls: ['https://evm-testnet.flowscan.io/'],
     chainId: 545,
@@ -35,7 +34,57 @@ const evmNetworks = [
     networkId: 545,
     rpcUrls: ['https://testnet.evm.nodes.onflow.org'],
     vanityName: 'Flow EVM Testnet',
-  }
+  },
+  {
+    blockExplorerUrls: ['https://etherscan.io/'],
+    chainId: 1,
+    chainName: 'Ethereum Mainnet',
+    iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+    name: 'Ethereum',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+      iconUrl: 'https://app.dynamic.xyz/assets/networks/eth.svg',
+    },
+    networkId: 1,
+
+    rpcUrls: ['https://mainnet.infura.io/v3/'],
+    vanityName: 'ETH Mainnet',
+  },
+  {
+    blockExplorerUrls: ['https://polygonscan.com/'],
+    chainId: 137,
+    chainName: 'Matic Mainnet',
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/polygon.svg"],
+    name: 'Polygon',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'MATIC',
+      symbol: 'MATIC',
+      iconUrl: 'https://app.dynamic.xyz/assets/networks/polygon.svg',
+    },
+    networkId: 137,
+    rpcUrls: ['https://polygon-rpc.com'],
+    vanityName: 'Polygon',
+  },
+  {
+    blockExplorerUrls: ['https://etherscan.io/'],
+    chainId: 5,
+    chainName: 'Ethereum Goerli',
+    iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+    name: 'Ethereum',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+      iconUrl: 'https://app.dynamic.xyz/assets/networks/eth.svg',
+    },
+    networkId: 5,
+    rpcUrls: ['https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
+
+    vanityName: 'Goerli',
+  },
 ];
 
 // Main App Component
